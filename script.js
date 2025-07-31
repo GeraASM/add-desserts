18122a2500446506ddb678252d8d97c7391621f3
GeraASM <gera2002h@gmail.com>
2025-07-30 20:45:02 -0700

    the part list dessert finish logical

---
+++ script.js
@@ -0,0 +1,220 @@
+let allDesserts;
+async function start() {
+    try {
+        const desserts = await fetch("./data.json").then(res => res.json());
+        allDesserts = desserts;
+
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+async function addDesserts() {
+    const btns = document.querySelectorAll(".btnCart");
+    btns.forEach(btn => {
+        btn.addEventListener("click", addDessert);
+    })
+}
+
+// Save Desserts
+let dessertsChose = [];
+const addDessert = (e) => {
+    const btnSelect = e.currentTarget;
+    const categoryName = btnSelect.dataset.dessert;
+    console.log(categoryName);
+    if (e.target.closest(".decrement")) {
+        const showCount = btnSelect.querySelector(".count");
+        removeCountDessert(categoryName, showCount);
+        return
+    } 
+    if (e.target.closest(".increment"))  {
+        const showCount = btnSelect.querySelector(".count");
+        addCountDessert(categoryName, showCount);
+        return 
+    } 
+    if (btnSelect.classList.contains("only")) {
+        btnSelect.classList.remove("only");
+        if (categoryName) appendDessert(categoryName);
+        btnSelect.classList.add("decrement-increment-btn");
+        btnSelect.innerHTML = `
+            <div>
+                    <svg class="circumference decrement" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 10 2">
+                        <path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/>
+                    </svg>
+            </div>
+            <p class="count">1</p>
+            <div>
+                    <svg class="circumference increment" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 10 10">
+                        <path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
+                    </svg>
+            </div>
+        `;
+
+    } 
+}
+
+function removeCountDessert(nameCategory, showCount) {
+    const index = dessertsChose.findIndex(d => d.category === nameCategory);
+    let countDessert = dessertsChose[index].number;
+    countDessert--;
+    if (countDessert > 0) {
+        dessertsChose[index].number = countDessert;
+        showCount.textContent = countDessert;
+    } else if (countDessert <= 0) {
+        dessertsChose[index].number = 0;
+        dessertsChose.splice(index, 1);
+        resetBtn(nameCategory);
+    }
+    updateList();
+    calculateAllTotal();
+    calculateCountDesserts();
+}
+
+function addCountDessert(nameCategory, showCount) {
+    const index = dessertsChose.findIndex(d => d.category === nameCategory);
+    let countDessert = dessertsChose[index].number;
+    countDessert++;
+    if (countDessert > 0) {
+        dessertsChose[index].number = countDessert;
+        showCount.textContent = countDessert;
+    } 
+    updateList();
+
+}
+
+
+function isInList(thisDessert) {
+    return dessertsChose.some(d => d.category == thisDessert.category);
+}
+
+function appendDessert(category) {
+    const dessert = allDesserts.find(d => d.category === category);
+    if (isInList(dessert)) {
+        const index = dessertsChose.findIndex(d => d.category == dessert.category);
+        dessertsChose[index].count++;
+    } else {
+        dessert.number = 1;
+        dessertsChose.push(dessert);
+    }
+    updateList();
+}
+
+const pictureCartEmpty = document.getElementById("figure"); 
+const block = document.getElementById("block");
+const numberDessertsInCart = document.getElementById("countDesserts");
+const listShow = document.getElementById("list");
+const totalShow = document.getElementById("allTotal");
+function updateList() {
+    if (dessertsChose.length > 0) {
+        pictureCartEmpty.style.display = 'none';
+        block.style.display = 'grid';
+        addToCartDesserts();
+    } else {
+        removeToCartDessers();
+    }
+    console.log(dessertsChose);
+}
+
+function resetBtn(nameCategory) {
+    const btns = document.querySelectorAll(".btnCart");
+    btns.forEach(b => {
+        if (b.dataset.dessert === nameCategory) {
+            b.classList.add("only");
+            b.classList.remove("decrement-increment-btn");
+            b.innerHTML = `
+                <img src="./assets/images/icon-add-to-cart.svg" alt="Add Cart">
+                <p>Add to Cart</p>
+            `
+        }
+    })
+}
+
+function calculateCountDesserts() {
+    let countDesserts = 0;
+    dessertsChose.forEach(d => countDesserts += d.number);
+    countShow.textContent = countDesserts;
+}
+
+
+function removeToCartDessers() {
+    listShow.innerHTML = '';
+    pictureCartEmpty.style.display = 'grid';
+    block.style.display = 'none';
+}
+
+
+function addToCartDesserts() {
+    listShow.innerHTML = '';
+    dessertsChose.forEach(dessert => {
+        const li = document.createElement("li");
+        li.className = "cart__item";
+        li.setAttribute("data-dessert", dessert.category);
+        li.innerHTML = `
+            <div class="cart__information">
+                    <h3 class="cart__name">${dessert.name}</h3>
+                    <div class="cart__prices">
+                        <p class="cart__number">${dessert.number}x</p>
+                        <p class="cart__base">@ $${(dessert.price).toFixed(2)}</p>
+                        <p class="cart__total">$${(dessert.price * dessert.number).toFixed(2)}</p>
+                    </div>
+                </div>
+                <div class="cart__close">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="none" viewBox="0 0 10 10">
+                        <path fill="currentColor" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/>
+                    </svg>
+                </div>
+        `
+        listShow.append(li);
+    })
+    calculateAllTotal();
+    calculateCountDesserts();
+}
+
+function calculateAllTotal() {
+    let total = 0;
+    dessertsChose.forEach(d => {
+        total += d.number * d.price;
+    })
+    
+    totalShow.textContent = `$ ${(total).toFixed(2)}`
+}
+
+function calculateCountDesserts() {
+    let count = 0;
+    dessertsChose.forEach(d => {
+        count += d.number;
+    })
+    numberDessertsInCart.textContent = count;
+}
+
+
+function deleteItem(e) {
+    const btns = document.querySelectorAll(".btnCart");
+    const item = e.target.closest(".cart__item");
+    const category = item.dataset.dessert;
+    console.log("Eliminar la categoria", category);
+    let btnDecrementIncrement;
+    btns.forEach(b => {
+        if (b.dataset.dessert == category) {
+            const showCount = b.querySelector(".count")
+            let number = Number(showCount.textContent);
+            for (number; number > 0; number--) {
+                removeCountDessert(category, showCount);
+                
+            }
+            console.log("The number count", number);
+        }
+    })
+   
+}
+
+listShow.addEventListener("click", (e) => {
+    if (e.target.closest(".cart__close")) {
+        deleteItem(e);
+    }
+})
+
+window.addEventListener("DOMContentLoaded", async () => {
+    await start();
+    await addDesserts();
+})
